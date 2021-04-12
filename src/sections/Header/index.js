96cc909771765fb25f98aec72efa0243a925e1ea
Radamanthus Batnag <radamanthus@gmail.com>
2021-04-12 20:47:20 +0800

    Update Header code

--- src/sections/Header/index.js
+++ src/sections/Header/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 import './styles.css'
 
-const Header = () => {
+const Header = ({title}) => {
   return (
-    <h1 class="header">This is the header</h1>
+    <h1 class="header">{title}</h1>
   )
 }
 
