616971922c03a703d63afafdd46ef7b84d1979b3
Radamanthus Batnag <radamanthus@gmail.com>
2021-06-08 14:57:16 +0800

    Add RemoveMe component

---
+++ src/components/RemoveMe/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './styles.css'
+
+/* Hello */
+/*
+ * @prop {string} fontSize - Font Size
+ */
+const RemoveMe = (fontSize) => {
+  return (
+    <p>Remove Me!</p>
+  )
+}
+
+export default RemoveMe
